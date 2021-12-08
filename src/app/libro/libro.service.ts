5452dedf7b49f1a0e1816b36af11bdc087f81593
amultari <a.multari@solvingteam.it>
2021-12-08 08:08:36 +0100

    Aggiunti campo data e boolean nel model

--- src/app/libro/libro.service.ts
+++ src/app/libro/libro.service.ts
@@ -8,10 +8,10 @@ import { Libro } from './libro';
 export class LibroService {
 
   LIBRI_MOCK: Libro[] = [
-    { id: 1, titolo: "Titolo1", pagine: 234, autore: 'Autore1' },
-    { id: 2, titolo: "Titolo2", pagine: 434, autore: 'Autore2' },
-    { id: 3, titolo: "Titolo3", pagine: 634, autore: 'Autore3' },
-    { id: 4, titolo: "Titolo4", pagine: 334, autore: 'Autore4' }
+    { id: 1, titolo: "Titolo1", pagine: 234, autore: 'Autore1', dataPubblicazione: new Date("2019-01-16"), premiato: false },
+    { id: 2, titolo: "Titolo2", pagine: 434, autore: 'Autore2', dataPubblicazione: new Date("1996-02-16"), premiato: true },
+    { id: 3, titolo: "Titolo3", pagine: 634, autore: 'Autore3', dataPubblicazione: new Date("1955-03-23"), premiato: false },
+    { id: 4, titolo: "Titolo4", pagine: 334, autore: 'Autore4', dataPubblicazione: new Date("2004-04-12"), premiato: false }
   ]
 
   constructor() { }
@@ -20,14 +20,14 @@ export class LibroService {
     return of(this.LIBRI_MOCK);
   }
 
-  getLibro(id:number):Observable<Libro>{
+  getLibro(id: number): Observable<Libro> {
     const libro = this.LIBRI_MOCK.find(libroItem => libroItem.id === id)!;
     return of(libro);
   }
 
   create(libroInput: Libro): Observable<Libro> {
-    const newId:number =Math.max.apply(Math, this.LIBRI_MOCK.map(libroItem => libroItem.id?libroItem.id:1));
-    libroInput.id = newId+1;
+    const newId: number = Math.max.apply(Math, this.LIBRI_MOCK.map(libroItem => libroItem.id ? libroItem.id : 1));
+    libroInput.id = newId + 1;
     this.LIBRI_MOCK.push(libroInput);
     return of(libroInput);
   }
--- src/app/libro/libro.ts
+++ src/app/libro/libro.ts
@@ -3,6 +3,8 @@ export class Libro {
         public id?: number,
         public titolo?: string,
         public pagine?: number,
-        public autore?: string
+        public autore?: string,
+        public dataPubblicazione?: Date,
+        public premiato: boolean = false
     ){}
 }
